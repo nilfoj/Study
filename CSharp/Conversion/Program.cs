5f181747ac1b76a27cf02362b1d3abc52b2da96d
nilfoj <nilfoj@hotmail.com>
2024-01-09 23:30:49 -0300

    ConvertingTypes

--- CSharp/Conversion/Program.cs
+++ CSharp/Conversion/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace App
+{
+    class Conversion
+    {
+        static void Main(string[] args)
+        {
+            int pvalor = 1;
+            float svalor = 2.1f;
+
+            //pvalor = svalor;
+            //svalor = (int)pvalor;
+
+            //string tvalor = pvalor.ToString();
+            //svalor = int.Parse(tvalor);
+
+            svalor = Convert.ToInt32(pvalor);
+
+            Console.WriteLine(svalor);
+            
+        }
+    }
+}
+
