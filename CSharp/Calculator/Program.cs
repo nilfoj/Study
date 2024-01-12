c6ef43be3cf2bd6070e0a833f4b21c9503ff6602
nilfoj <nilfoj@hotmail.com>
2024-01-11 23:35:30 -0300

    Calculator
    Simple calculator with some operations - C# Course - Balta.io

---
+++ CSharp/Calculator/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Menu();
+            
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("Choose which mathematical operation you want to perform?");
+            Console.WriteLine("1 – Sum");
+            Console.WriteLine("2 - Subtraction");
+            Console.WriteLine("3 - Multiplication");
+            Console.WriteLine("4 - Division");
+            Console.WriteLine("5 - Exit!");
+            Console.WriteLine("Choose an option:");
+
+
+            short result = short.Parse(Console.ReadLine());
+            switch (result)
+            {
+
+                case 1: Sum(); break;
+                case 2: Subtraction(); break;
+                case 3: Multiplication(); break;
+                case 4: Division(); break;
+                case 5: System.Environment.Exit(0); break;
+                default: Menu(); break;
+                
+            }
+
+        }
+
+        static void Sum()
+        {
+            
+            Console.Clear();
+            
+            Console.WriteLine("First value:");
+            double v1 = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Second value:");
+            double v2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Result = {v1 + v2}");
+            Console.WriteLine();
+           
+            Menu();
+        }
+
+        static void Subtraction()
+        {
+            Console.Clear();
+          
+            Console.WriteLine("First value:");
+            double v1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Second value:");
+            double v2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Result = {v1 - v2}");
+            Console.WriteLine();    
+
+            Menu();
+
+        }
+
+        static void Multiplication()
+        {
+            Console.Clear();
+
+            Console.WriteLine("First value:");
+            double v1 = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Second value:");
+            double v2 = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Result = {v1 * v2}");
+            Console.WriteLine();
+
+            Menu();
+        }
+
+        static void Division()
+        {
+            Console.Clear();
+
+            Console.WriteLine("First value:");
+            double v1 = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Second value:");
+            double v2 = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Result = {v1 / v2}");
+            Console.WriteLine();
+
+            Menu();
+
+        }
+
+    }
+}
--- CSharp/Operators/Program.cs
+++ CSharp/Operators/Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        //Soma +
+        //Subtração -
+        //Multiplicação *
+        //Divisão /
+
+
+        int valor01 = 1 + 1;
+        int valor02 = 2 - 1;
+        int valor03 = 3 * 1;
+        int valor04 = 4 / 1;
+
+        Console.WriteLine(valor01);
+        Console.WriteLine(valor02);
+        Console.WriteLine(valor03); 
+        Console.WriteLine(valor04);
+
+
+        int x = 0;
+      
+        Console.WriteLine(x += 5);
+        Console.WriteLine(x -= 1);
+        Console.WriteLine(x *= 2);
+        Console.WriteLine(x /= 1);
+
+
+        //Comparação
+        // Igual ==
+        //Diferente != 
+        // Maior que >
+        //Menor que <
+        //Maior ou igual <=
+        //Menor our igual >=
+
+        Console.WriteLine(x < 24);
+        Console.WriteLine(x >= 24);
+
+
+        //Operadores Lógicos
+        // and = &&
+        // or = ||
+        // not = !
+
+        int z = 0;
+        bool entre = (z > 25) && (z < 40);
+        bool ou = (z > 25) || (z < 40);
+        bool negacao = !(z < 25);
+
+        Console.WriteLine(entre);
+        Console.WriteLine(ou);
+        Console.WriteLine(negacao);
+
+
+        //Estrutura de Condição If e Else
+        // if
+        // else
+        // else if
+
+
+
+
+
+
+    }
+}
