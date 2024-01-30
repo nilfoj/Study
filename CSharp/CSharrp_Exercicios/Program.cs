3339cd44f85481829b5fa61cef062aa8de7c5fa5
nilfoj <nilfoj@hotmail.com>
2024-01-29 23:12:13 -0300

    CSharp_Execicios

--- CSharp/CSharrp_Exercicios/Program.cs
+++ CSharp/CSharrp_Exercicios/Program.cs
@@ -7,9 +7,11 @@ namespace Exercicio
         static void Main(string[] args)
         {
 
-            //Hello();
-            //Nome();
-            Soma();
+            //001Hello(); 
+            //002Nome();
+            //003Soma();
+            //005PredSecce();
+            //006DTRaiz();
 
         }
 
@@ -37,7 +39,28 @@ namespace Exercicio
             Console.WriteLine($"A soma entre {valor01} e {valor02} é igual a {valor01 + valor02}");
         }
 
+        static void PredSecce()
+        {
+            Console.Write("Digite um número: ");
+            int valor01 = int.Parse(Console.ReadLine());
 
+            Console.WriteLine($"Analisando o valor {valor01}, seu antecessor é {valor01 - 1} e o sucessor é {valor01 + 1}");
+       
+        }
+
+        static void DTRaiz()
+        {
+            Console.Write("Digite um valor: ");
+            int valor01 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O dobro de {valor01} vale {valor01*2}.");
+            Console.WriteLine($"O dobro de {valor01} vale {valor01 * 3}.");
+
+            double valor02 = Math.Sqrt(valor01);
+            string formato = valor02.ToString("0.00");
+
+            Console.WriteLine($"A raiz quadrada de {valor01} é igual a {formato}");
+        }
 
     }
 }
