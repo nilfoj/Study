9e50b896f7c737844c65ff8b018e71fd1c3fe249
nilfoj <nilfoj@hotmail.com>
2024-02-28 23:08:10 -0300

    Alura_Project_03

---
+++ CSharp/List_Matriz/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            
+            List<int> listaNumeros = new List<int>();
+            
+            listaNumeros.Add(1);
+            listaNumeros.Add(2);
+
+            Console.WriteLine(listaNumeros[0]);
+        }
+    }
+}
