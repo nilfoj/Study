ab6185cc3c8776eb27f4daf89f3c382c54168c4c
nilfoj <nilfoj@hotmail.com>
2024-03-04 22:45:38 -0300

    CSharp_Study_List

--- CSharp/List_Matriz/Program.cs
+++ CSharp/List_Matriz/Program.cs
@@ -6,20 +6,72 @@ namespace list
     {
         static void Main(string[] args)
         {
+
+            List();
+
+        }
+
+        static void List()
+        {
             Console.Clear();
 
             List<int> listaNumeros = new List<int>();
 
-            int valores = Convert.ToInt16(Console.ReadLine());
-            
-            for(int i = 0; i < valores; i++)
+            int valores = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < valores; i++)
             {
                 int numero = Convert.ToInt16(Console.ReadLine());
                 listaNumeros.Add(numero);
             }
-           
-            foreach(int numero in listaNumeros) { Console.WriteLine(numero); }
+
+
+            //foreach (int numero in listaNumeros) {
+            //    Console.WriteLine(numero); 
+            //}
+
+
+            for (int i = 0; i < listaNumeros.Count; i++)
+            {
+                Console.WriteLine("Posição {0}:{1}", i, listaNumeros[i]);
+            }
+
+
+            int soma = 0;
+
+            for (int i = 0; i < listaNumeros.Count; i++)
+            {
+                soma += listaNumeros[i];
+            }
+
+            Console.WriteLine("A soma dos numeros da list é {0}", soma);
+
+
+            Console.Write("Digite a posição da lista que deseja trocar:");
+            int posicao = int.Parse(Console.ReadLine());
+
+            if (posicao < 0 || posicao >= listaNumeros.Count)
+            {
+                Console.WriteLine("Inválido!");
+                return;
+            }
+
+            Console.Write("Digite o numero que deseja colocar:");
+            int numeronovo = int.Parse(Console.ReadLine());
+
+            listaNumeros[posicao] = numeronovo;
+
+            for (int i = 0;i < listaNumeros.Count; i++)
+            {
+                Console.WriteLine(listaNumeros[i]);
+            }
 
         }
+
+
+
+
+  
+
     }
 }
