fdd050fb53e8e45992807eb002d70e7b3e930966
nilfoj <nilfoj@hotmail.com>
2024-03-15 01:14:18 -0300

    CSharp_Matriz

---
+++ CSharp/Matriz/Program.cs
@@ -0,0 +1,90 @@
+﻿//Declarando a matriz
+
+/*
+ 
+ M {A1 B1 C1
+    D2 E2 F2
+    G3 H3 I3}
+ 
+ */
+
+//Escrevendo Matriz
+
+int[,] matriz = new int[3, 3];
+
+for (int i = 0; i < matriz.GetLength(0); i++) 
+{
+    for (int j = 0; j < matriz.GetLength(1); j++)
+    {
+        Console.WriteLine("Digite o elemento [{0}.{1}]:", i, j);
+        matriz[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+Console.WriteLine();
+
+
+//Imprimindo a Matriz
+
+for (int i = 0; i < matriz.GetLength(0); i++)
+{
+
+    for (int j = 0; j < matriz.GetLength(1); j++)
+    {
+        Console.Write(matriz[i, j] + " ");
+    }
+
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+
+//Somando Linha 1 
+
+int somaLinha1 = 0;
+
+for (int i = 0; i < matriz.GetLength(1); i++)
+{
+    somaLinha1 += matriz[0, i];
+}
+
+Console.WriteLine(somaLinha1);
+Console.WriteLine();
+
+
+//Somando Linha 2
+
+int somaLinha2 = 0;
+
+for (int i = 0; i < matriz.GetLength(1); i++)
+{
+    somaLinha2 += matriz[1, i];
+}
+
+Console.WriteLine(somaLinha2);
+Console.WriteLine();
+
+
+//Somando Linha 3
+int somaLinha3 = 0;
+
+for (int i = 0; i < matriz.GetLength(1); i++)
+{
+    somaLinha3 += matriz[2, i];
+}
+
+Console.WriteLine(somaLinha3);
+Console.WriteLine();
+
+
+//Somando Coluna 1
+int somaColuna1 = 0;
+
+for (int j = 0; j < matriz.GetLength(2); j++)
+{
+    somaColuna1 += matriz[0, j];
+}
+
+Console.WriteLine(somaColuna1);
+Console.WriteLine();
