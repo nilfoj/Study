469b7b0393b795a38621e0c98a15632d50446b27
nilfoj <nilfoj@hotmail.com>
2024-03-14 23:30:45 -0300

    CSharp_Backend

---
+++ CSharp/Classes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine();
+            var casinha = new Class1();
+           
+
+
+
+        }
+    }
+}
