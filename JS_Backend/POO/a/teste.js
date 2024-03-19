14d3d084cae00c89d42c61e23cf18547ae4bd6d6
nilfoj <nilfoj@hotmail.com>
2024-03-18 23:00:30 -0300

    Study_CSharp_Att

--- JS_Backend/POO/a/teste.js
+++ JS_Backend/POO/a/teste.js
@@ -1,3 +1,67 @@
 const dados = require("./client.json");
 
 console.log(dados);
+
+
+const cliente = {
+    nome: "Joao",
+    idade: 23,
+    email: "joao@gmail.com",
+    telefone: ["00000000", "11111111"],
+};
+
+
+
+cliente.enderecos = [
+{	
+    
+    rua: "Rua Projetada",
+	numero: 123,
+	apartamento: true,
+	complemento: "Ap Pequeno",
+    
+},
+];
+
+
+cliente.enderecos.push({
+    rua: "Rua Projetada 2",
+	numero: 456,
+	apartamento: false,
+	complemento: "Casa Pequena",
+});
+
+console.log(cliente.enderecos)
+
+
+const listaApartamentos = cliente.enderecos.filter(
+    (endereco) => endereco.apartamento === true 
+);
+
+
+
+
+
+const cliente = {
+    nome: "Joao",
+    idade: 50,
+    telefone: 0000000,
+    saldo: 100,
+    
+    Pagamento: function (valor) {
+        
+        if (valor > this.saldo) {
+            console.log("Salda Insuficinete.")
+        }
+
+        else {
+            this.saldo -= valor;
+            console.log(`Pagamento realizado. Novo saldo: ${this.saldo}`); 
+        }
+    },
+
+
+};
+
+cliente.Pagamento(120);
+cliente.Pagamento(10);
